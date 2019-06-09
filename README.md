6e8ca29c95f5da437e696e9c70f0a1f3894ac43b
Shane Hird <shanehird@gmail.com>
2019-06-09 20:35:08 +1000

    Use code-lens to idenfity tests

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# console-log-highlighter README
+# epmtestharness-runner
 
-An example extension to show how to use the Decorations API in Visual Studio Code
-
-This example is used in the [Decorations](https://vscode.rocks/decorations/) Article on VS Code Rocks
+Provides a code lens to run individual epmtestharness tests.
--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,66 @@
 import * as vscode from "vscode";
+import {
+  commands,
+  languages,
+  CodeLensProvider,
+  TextDocument,
+  CodeLens,
+  Command
+} from "vscode";
 
-const decorationType = vscode.window.createTextEditorDecorationType({
-  backgroundColor: "green",
-  border: "2px solid white"
-});
+class MyCodeLensProvider implements CodeLensProvider {
+  async provideCodeLenses(document: TextDocument): Promise<CodeLens[]> {
+    const sourceCode = document.getText();
+    const regex = /(test_[a-zA-Z0-9]+)\&\(\) =/;
+    const sourceCodeArr = sourceCode.split("\n");
+    let codeLenses: vscode.CodeLens[] = [];
+
+    for (let line = 0; line < sourceCodeArr.length; line++) {
+      const match = sourceCodeArr[line].match(regex);
+      if (match !== null && match.index !== undefined) {
+        const range = new vscode.Range(
+          new vscode.Position(line, match.index),
+          new vscode.Position(line, match.index + match[1].length)
+        );
+        const c: Command = {
+          command: "extension.runTest",
+          title: ">> Run test",
+          arguments: [match[1]],
+        };
+        const codeLens = new CodeLens(range, c);
+        codeLenses.push(codeLens);
+      }
+    }
+    return codeLenses;
+  }
+}
+
+async function runTest(testName) {
+  // TODO: exec epmtestharness testName task
+  console.log('running test ' + testName);
+}
 
 export function activate(context: vscode.ExtensionContext) {
-  vscode.workspace.onWillSaveTextDocument(event => {
-    const openEditor = vscode.window.visibleTextEditors.filter(
-      editor => editor.document.uri === event.document.uri
-    )[0];
-    decorate(openEditor);
-  });
-}
+  // Register the command
+  let commandDisposable = commands.registerCommand(
+    "extension.runTest",
+    runTest
+  );
 
-function decorate(editor: vscode.TextEditor) {
-  let sourceCode = editor.document.getText();
-  let regex = /(console\.log)/;
-
-  let decorationsArray: vscode.DecorationOptions[] = [];
-
-  const sourceCodeArr = sourceCode.split("\n");
-
-  for (let line = 0; line < sourceCodeArr.length; line++) {
-    let match = sourceCodeArr[line].match(regex);
-
-    if (match !== null && match.index !== undefined) {
-      let range = new vscode.Range(
-        new vscode.Position(line, match.index),
-        new vscode.Position(line, match.index + match[1].length)
-      );
-
-      let decoration = { range };
-
-      decorationsArray.push(decoration);
-    }
+  let docSelector = {
+    language: 'epm',
+    scheme: 'file'
   }
 
-  editor.setDecorations(decorationType, decorationsArray);
+  // Register our CodeLens provider
+  let codeLensProviderDisposable = languages.registerCodeLensProvider(
+    docSelector,
+    new MyCodeLensProvider()
+  );
+
+  // Push the command and CodeLens provider to the context so it can be disposed of later
+  context.subscriptions.push(commandDisposable);
+  context.subscriptions.push(codeLensProviderDisposable);
 }
+
+export function deactivate() {}
